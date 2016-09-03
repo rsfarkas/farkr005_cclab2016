41c36e44d5a367cf14d4f07088fe4a81ca48138e
Roxanne Farkas <roxannesfarkas@gmail.com>
2016-09-03 16:12:28 -0400

    JS animation upload

---
+++ Frog/assets/main.js
@@ -0,0 +1,6 @@
+
+var changeFrog = function(){
+  document.getElementById("animatedfrog");
+  console.log("animatedfrog");
+  document.getElementById("animatedfrog").className = "animated bounce";
+}
