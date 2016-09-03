39a29978d7527d0b3ca5dc038f4271e08da328b2
Roxanne Farkas <roxannesfarkas@gmail.com>
2016-09-03 16:17:35 -0400

    removed console.log and moved css to correct folder

--- Frog/assets/main.js
+++ Frog/assets/main.js
@@ -1,6 +1,5 @@
 
 var changeFrog = function(){
   document.getElementById("animatedfrog");
-  console.log("animatedfrog");
   document.getElementById("animatedfrog").className = "animated bounce";
 }
