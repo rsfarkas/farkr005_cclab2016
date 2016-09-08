841a104ae38d9f2297e99e1f522ab2a782e26cd0
Roxanne Farkas <roxannesfarkas@gmail.com>
2016-09-08 14:46:21 -0400

    basic pulsing

--- jqueryanimation/js/main.js
+++ jqueryanimation/js/main.js
@@ -1,21 +1,39 @@
-
-$(document).ready(function(){
-	$('img').click(function() {
-  		$('img').fadeOut("slow");
-  		// $('img').replaceWith('<div id="clock"></div>');
-	});
+var clock = $('.clock').FlipClock(new Date(2018,5,20).getTime()/1000 - new Date().getTime()/1000, {
+	clockFace: 'DailyCounter',
+	countdown: true
 });
 
 
 
-	// var clock = $('.clock').FlipClock(743000 * 24 * 3, {
-	// 	clockFace: 'DailyCounter',
-	// 	countdown: true
-	// });
-  
+$(document).ready(function(){
+	$('img').click(function() {
+		$('img').fadeOut("slow");
+		console.log("not replaced");
+		$('img').replaceWith('<img src="img/giphy.gif"><img>');
+		console.log("replaced");
+	});
 
-  var clock = $('.clock').FlipClock(new Date(2018,8,30).getTime()/1000 - new Date().getTime()/1000, {
-            clockFace: 'DailyCounter',
-            countdown: true
-        });
+// 	$('#img').hover(function(){
+// 	$(this).css({width:"110%",height:"110%"});
+// })();
+
+$( "#img" ).hover(function() {
+  $( this ).css({width:"95%", height:"95%"});
+},function(){
+    $(this).css({width:"100%",height:"100%"});  
+});
+
+	// $('#img').hover(function(){
+	// 	$(this).css({width:"110%",height:"110%"});
+	// },function(){
+	// 	$(this).css({width:"100%",height:"100%"});   
+	// })();
+
+
+});
+
+
+
+
+
 
