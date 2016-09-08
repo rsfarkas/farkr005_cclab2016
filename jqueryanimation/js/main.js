d68a4406f322460e9f749fdecc66ca9f126488e4
Roxanne Farkas <roxannesfarkas@gmail.com>
2016-09-08 10:53:03 -0400

    basic working timer

--- jqueryanimation/js/main.js
+++ jqueryanimation/js/main.js
@@ -5,3 +5,10 @@ $(document).ready(function(){
   		$('img').replaceWith('<div id="countdown">hayyyy</div>');
 	});
 });
+
+
+
+	var clock = $('.clock').FlipClock(3600 * 24 * 3, {
+		clockFace: 'DailyCounter',
+		countdown: true
+	});
