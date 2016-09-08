8fa450192841c6f708fb712a3518256df422c541
Roxanne Farkas <roxannesfarkas@gmail.com>
2016-09-08 12:13:13 -0400

    clock working

--- jqueryanimation/js/main.js
+++ jqueryanimation/js/main.js
@@ -2,14 +2,20 @@
 $(document).ready(function(){
 	$('img').click(function() {
   		$('img').fadeOut("slow");
-  		 		$('#clock').addClass("clock");
-  		$('img').replaceWith('<div id="clock"></div>');
+  		// $('img').replaceWith('<div id="clock"></div>');
 	});
 });
 
 
 
-	var clock = $('.clock').FlipClock(743000 * 24 * 3, {
-		clockFace: 'DailyCounter',
-		countdown: true
-	});
+	// var clock = $('.clock').FlipClock(743000 * 24 * 3, {
+	// 	clockFace: 'DailyCounter',
+	// 	countdown: true
+	// });
+  
+
+  var clock = $('.clock').FlipClock(new Date(2018,8,30).getTime()/1000 - new Date().getTime()/1000, {
+            clockFace: 'DailyCounter',
+            countdown: true
+        });
+
